b2d6b3788cb5ef674455589ed673805c6d1fc4dc
yago <yago@MINOS>
2018-04-28 13:17:49 -0300

    Se agregan los archivos csv y se modifican constantes

--- src/Utils/Const.java
+++ src/Utils/Const.java
@@ -13,7 +13,11 @@ public class Const {
     
     public class Files
     {
-        public static final String Books = "Libros.txt";
+        public static final String Books = "src/CSVFiles/books.csv";   
+        public static final String Authors = "src/CSVFiles/authors.csv";    
+        public static final String BookAutors = "src/CSVFiles/bookauthors.csv";
+        public static final String BookTags = "src/CSVFiles/booktags.csv";     
+        public static final String Tags = "src/CSVFiles/tags.csv";
     }
     
     public class CSV
